4d1b70dfdba74a16d5ed4d7025f96209a0799f06
Russell F <rframe83@gmail.com>
2017-02-14 16:05:11 -0800

    Styling Components with ngStyle, ngStyle binding

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -10,7 +10,7 @@ import {Component, Input} from '@angular/core'
             <h2>{{event?.name}}</h2>
             <div>Date: {{event?.date}}</div>
             
-            <div [style.color]="event?.time === '8:00 am' ? '#003300' : '#bbb'" [ngSwitch]="event?.time">Time: {{event?.time}}
+            <div [ngStyle]="{'color': event?.time === '8:00 am' ? '#003300' : '#bbb', 'font-weight': event?.time === '8:00 am' ? 'bold' : 'normal'}" [ngSwitch]="event?.time">Time: {{event?.time}}
                 <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
                 <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
                 <span *ngSwitchDefault>(Normal Start)</span>
