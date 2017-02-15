9c4cf7ecbeb2eba4514e0fab6e91a2ae0ef10442
Russell F <rframe83@gmail.com>
2017-02-14 16:08:16 -0800

    Styling Components with ngStyle

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -10,7 +10,7 @@ import {Component, Input} from '@angular/core'
             <h2>{{event?.name}}</h2>
             <div>Date: {{event?.date}}</div>
             
-            <div [ngStyle]="{'color': event?.time === '8:00 am' ? '#003300' : '#bbb', 'font-weight': event?.time === '8:00 am' ? 'bold' : 'normal'}" [ngSwitch]="event?.time">Time: {{event?.time}}
+            <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">Time: {{event?.time}}
                 <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
                 <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
                 <span *ngSwitchDefault>(Normal Start)</span>
@@ -44,14 +44,22 @@ export class EventThumbnailComponent {
         const isEarlyStart = this.event && this.event.time === '8:00 am';
         return {green: isEarlyStart, bold: isEarlyStart};
 
-        // if(this.event && this.event.time === '8:00 am') {
+        // if(isEarlyStart) {
         //     return ['green', 'bold'];
         // }
         // return [];
 
-        // if(this.event && this.event.time === '8:00 am') {
+        // if(isEarlyStart) {
         //     return 'green bold';
         // }
         // return '';
+    }
+
+    getStartTimeStyle(): any {
+        const isEarlyStart = this.event && this.event.time === '8:00 am';
+        if(isEarlyStart) {
+            return {color: '#003300', 'font-weight': 'bold'};
+        }
+        return {};
     }
 }
